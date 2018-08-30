6a5cf334e208abd52bccad03196970c391cd0286
elgun <elgun1999@gmail.com>
2018-08-30 22:35:36 +0400

    modified mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://<elgun99>:<elgunCebo123>@ds139722.mlab.com:39722/todo' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
