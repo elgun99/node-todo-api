12f29b2dfe2c59a8e34b295f649141ca4cfee85a
elgun <elgun1999@gmail.com>
2018-09-01 19:16:59 +0400

    Add test for DELETE route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -284,3 +284,22 @@ describe('POST /users/login', () => {
       });
   });
 })
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    var user = users[0];
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', user.tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err){
+          return done(err);
+        }
+        User.findById(user._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      })
+  })
+});
